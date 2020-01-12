91e6c9687320fa682a6ba9bd2963943a6e04123f
karthikeyan <karthikeyan@bangthetable.com>
2020-01-13 00:25:08 +0530

    Finished the v1.0 of the app
    1. generated the timeline using github api
    2. sorted the date created repo in descending order
    3. made the table structure instead of the single line as it does not
    cover most of the scenarios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
+class App extends React.Component {
+  state = {
+    username: "",
+    repoDetails: []
+  };
+  ChangeUsername = e => {
+    this.setState({
+      username: e.target.value
+    });
+  };
+  getUsername = e => {
+    e.preventDefault();
+    fetch(`https://api.github.com/users/${this.state.username}/repos`)
+      .then(res => {
+        if (!res.ok) throw Error(res.statusText);
+        return res.json();
+      })
+      .then(res => {
+        this.setState({
+          repoDetails: res
+        });
+      })
+      .catch(res => console.log("errro", res));
+  };
+  render() {
+    return (
+      <div className="Timeline-outer__block container-fluid">
+        <nav className="navbar navbar-light bg-dark">
+          <a className="navbar-brand" href="/">
+          <FontAwesomeIcon icon={faGithub}  /> Github Timeline
+          </a>
+        </nav>
+        <br/>
+        <div className="jumbotron container-fluid">
+        <form>
+        <div class="form-row justify-content-center">
+          <div class="col-3">
+          <input type="text" class="form-control" placeholder="Enter your github username" onChange={this.ChangeUsername}/>
+          </div>
+          <div class="col-2 ">
+          <button className='form-control' onClick={this.getUsername}> Generate </button>
+          </div>
+         </div>
+       </form>
+        </div>
+        
+       <br/>
+       {
+        
+        this.state.repoDetails.length !== 0 ?
+        <Timeline repo = {this.state.repoDetails} username={this.state.username} />
+        : 
+        <p className='text-center'> Enter the github username to generate the timeline </p> 
+        }
+       </div>
+    );
+  }
+}
+export default App;
+
+const Timeline = ({ repo , username }) => {
+  let Repos = repo.sort((a, b) => new Date(b.created_at).getFullYear() - new Date(a.created_at).getFullYear())
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Timeline for {username} </p>
+     
+        <table className="table">
+        <thead>
+        <tr>
+        <th scope="col"> Repo - Name </th>
+        <th scope="col"> Description </th>
+        <th scope="col"> Year </th>
+        </tr>
+        </thead>
+        <tbody>
+        
+        {
+          Repos.map(repo=>{
+          return(
+            <tr>
+            <td> { repo.name } </td>
+            <td> { repo.description !== null ? repo.description : <p className='text-muted'> No description </p> } </td>
+            <td> { new Date(repo.created_at).getFullYear() }</td>
+            </tr>
+          )
+        })
+         
+      }
+      </tbody>
+      </table>
     </div>
   );
-}
-
-export default App;
+};
