750ebb919e0638b6d0d4a7a06fd1c4d8c110c27d
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-03-16 18:34:46 +0530

    fix: checking for sentry release

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/tracing";
 
 Sentry.init({
-    dsn: "https://d9d7d0558be6438fa6492ea966f2cfa5@o1117361.ingest.sentry.io/6261831",
+    dsn: "https://3e4c24f451e240c59dae23992a67e5d6@o1117361.ingest.sentry.io/6261910",
     integrations: [new BrowserTracing()],
     environment: process.env.NODE_ENV,
     // Set tracesSampleRate to 1.0 to capture 100%
